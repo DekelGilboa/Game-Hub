02f35746d46e1f0eca2f814881023f6333ecfe22
dekel <dekel009@gmail.com>
2024-02-26 22:51:11 +0200

    add resposive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import './App.css'
-import { Heading } from '@chakra-ui/react'
+import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-    <Heading>My App</Heading>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"}>nav</GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"}>aside</GridItem>
+        </Show>
+        <GridItem area={"main"}>main</GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
