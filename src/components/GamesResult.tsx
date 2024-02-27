e9cdfc01ec16a373e93180e02d39718ef5df5ebf
dekel <dekel009@gmail.com>
2024-02-27 14:55:43 +0200

    create custom hook to fetch games, saved it on localStorage and define an abortController

--- src/components/GamesResult.tsx
+++ src/components/GamesResult.tsx
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchedGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GamesResult = () => {
-  const [gamesList, setGameList] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchedGamesResponse>("/games")
-      .then((res) => {
-        setGameList(res.data.results);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  });
-
+  const { gamesList, error } = useGames();
   return (
     <>
       {error && <div>{error}</div>}
       <ul>
-        {gamesList.map(({id, name}) => (
+        {gamesList.map(({ id, name }) => (
           <li key={id}>{name}</li>
         ))}
       </ul>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/apiClient";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchedGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [gamesList, setGameList] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const gameListAsStr = localStorage.getItem("gameList") || "[]";
+    if (gameListAsStr == "[]") {
+      apiClient
+        .get<FetchedGamesResponse>("/games", { signal: controller.signal })
+        .then((res) => {
+          const gameListAsStr = JSON.stringify(res.data.results);
+          localStorage.setItem("gameList", gameListAsStr);
+          setGameList(JSON.parse(gameListAsStr));
+        })
+        .catch((err) => {
+          return err instanceof CanceledError ? null : setError(err.message);
+        });
+    } else {
+      setGameList(JSON.parse(gameListAsStr));
+    }
+    return () => controller.abort();
+  }, []);
+
+  return { gamesList, error };
+};
+
+export default useGames;
