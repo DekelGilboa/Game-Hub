00e4e7e6d1c9971488c15784d126ac51886d467c
dekel <dekel009@gmail.com>
2024-02-27 19:42:34 +0200

    add unique key for each Icon

--- src/components/PlatformsIcons.tsx
+++ src/components/PlatformsIcons.tsx
@@ -1,7 +1,14 @@
-import {FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid} from "react-icons/fa"
-import {MdPhoneIphone} from "react-icons/md"
-import {SiNintendo} from "react-icons/si"
-import {BsGlobe} from "react-icons/bs"
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
@@ -10,24 +17,23 @@ interface Props {
   platforms: Platform[];
 }
 
-const slugToIconsMap: {[key: string]: IconType} = {
-        pc: FaWindows,
-        playstation: FaPlaystation,
-        xbox: FaXbox,
-        mac: FaApple,
-        linox: FaLinux,
-        android: FaAndroid,
-        ios: MdPhoneIphone,
-        nintendo: SiNintendo,
-        web: BsGlobe
-
+const slugToIconsMap: { [key: string]: IconType } = {
+  pc: FaWindows,
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  mac: FaApple,
+  linox: FaLinux,
+  android: FaAndroid,
+  ios: MdPhoneIphone,
+  nintendo: SiNintendo,
+  web: BsGlobe,
 };
 
 const PlatformsIcons = ({ platforms }: Props) => {
   return (
     <HStack justifyContent={"center"} paddingY={"0.2rem"}>
-      {platforms.map((p) => (
-        <Icon as={slugToIconsMap[p.slug]} color={"gray.500"}></Icon>
+      {platforms.map(({ id, slug }) => (
+        <Icon key={id} as={slugToIconsMap[slug]} color={"gray.500"}></Icon>
       ))}
     </HStack>
   );
