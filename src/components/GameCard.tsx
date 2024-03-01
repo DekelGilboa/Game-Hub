c44441156f7c696d0cbcc68e70ca08289655770e
dekel <dekel009@gmail.com>
2024-03-01 13:27:46 +0200

    add game's score badge

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,20 +1,24 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformsIcons from "./PlatformsIcons";
+import GameScore from "./GameScore";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({
-  game: { name, background_image, parent_platforms},
+  game: { name, background_image, parent_platforms, metacritic },
 }: Props) => {
   return (
     <Card overflow={"hidden"}>
       <Image src={background_image} />
       <CardBody>
         <Heading textAlign={"center"} fontSize={"xl"} marginBottom={"0.4rem"}>{name}</Heading>
+        <HStack justifyContent={"space-between"}>
         <PlatformsIcons platforms={parent_platforms.map(p=>p.platform)} />
+        <GameScore score={metacritic}/>
+        </HStack>
       </CardBody>
     </Card>
   );
---
+++ src/components/GameScore.tsx
@@ -0,0 +1,24 @@
+import { Badge, useColorMode } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const GameScore = ({ score }: Props) => {
+  const { colorMode } = useColorMode();
+  const color = score > 80 ? "green" : score > 60 ? "orange" : "red";
+  return (
+    <Badge
+      fontSize={"0.8em"}
+      borderRadius={"0.2rem"}
+      colorScheme={color}
+      variant={colorMode == "dark" ? "outline" : "subtle"}
+      paddingX={"0.5em"}
+      paddingY={"0.1em"}
+    >
+      {(score / 10).toFixed(1)} / 10
+    </Badge>
+  );
+};
+
+export default GameScore;
