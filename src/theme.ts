1c6e1c02892652eb93db4f85a50a4d1236d529f8
dekel <dekel009@gmail.com>
2024-03-25 17:27:54 +0200

    fix light theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,28 @@
 // 1. import `extendTheme` function
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 // 2. Add your color mode config
 const config: ThemeConfig = {
-  initialColorMode: 'system',
-}
+  initialColorMode: "system",
+};
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  colors: {
+    white: "#EDF2F7",
+    gray: {
+      100: "#E2E8F0",
+      200: "#CBD5E0",
+      300: "#A0AEC0",
+      400: "#718096",
+      500: "#4A5568",
+      600: "#2D3748",
+      700: "#1A202C",
+      800: "#171923",
+      900: "#1A202C",
+    },
+  },
+});
 
-export default theme
+export default theme;
